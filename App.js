62378ce8e5d5926168c20fa9302561c712d3d09a
Milan-Koladiya <developer.milankoladiya@gmail.com>
2021-12-25 14:25:51 +0530

    Added textListing

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Button, TextInput, FlatList, ScrollView, Text } from 'react-native';
 
 export default function App() {
+  const [inputText, setInputText] = useState("test");
+  const [textList, setTextList] = useState([]);
+
+  const handleSubmit = () => {
+    setInputText("");
+    setTextList([...textList, inputText]);
+  }
+
+  console.log("textList => ", textList);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="test"></Button>
-      <StatusBar style="auto"/>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputText}  placeholder="Please input text" value={inputText} onChangeText={setInputText}/>
+        <Button onPress={handleSubmit} style={styles.submitButton} title="Submit"/>
+      </View>
+      <View>
+        <ScrollView>
+          {textList.map((text, index) => 
+            <View style={styles.listView}>
+              <Text key={index}>{text}</Text>
+            </View>
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -17,5 +37,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center', 
     justifyContent: 'center',
+    marginTop: 100
   },
+  submitButton: {
+    marginTop: 10
+  },
+  inputText: {
+    borderColor: "red",
+    borderRadius: 1
+  },
+  listView: {
+    padding: 10,
+    width: 80,
+    backgroundColor: 'red',
+    borderColor: 'red'
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
